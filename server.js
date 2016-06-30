ec0d137a653d234fc6b6744130bf8e6e6a607c8d
Vincent Zheng <zhengye1@mail.utoronto.ca>
2016-06-30 18:05:37 -0400

    No error on coding, but cannot open the index page

---
+++ server.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var express = require('express'),
+    stylus = require('stylus'),
+    bodyParser = require('body-parser'),
+    logger = require('morgan'),
+    path = require('path');
+
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var app = express();
+
+function compile(str, path) {
+    return stylus(str).set('filename', path);
+};
+
+app.set('views', __dirname  + '/server/views');
+app.set('view engine', 'jade');
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(stylus.middleware(
+    {
+        src : __dirname + '/public',
+        compile: compile
+    }
+));
+app.use(express.static(__dirname  + '/server/views'));
+
+
+app.get('*', function(req, res){
+    res.render('index');
+});
+
+var port = 3030;
+app.listen(port);
+console.log('Listening on port ' + port + '...');
