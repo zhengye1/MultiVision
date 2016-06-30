792b9d2c7f15b45b7ebfc40b71ac9288df16ff4d
Vincent Zheng <zhengye1@mail.utoronto.ca>
2016-06-30 19:54:03 -0400

    Add back app.get('/partials/:partialPath', function(req, res){              res.render('/partials/' + req.param.partialPath);          });

--- server.js
+++ server.js
@@ -14,22 +14,27 @@ function compile(str, path) {
     return stylus(str).set('filename', path);
 };
 
+
 app.set('views', __dirname  + '/server/views');
 app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(stylus.middleware(
-    {
-        src : __dirname + '/public',
+app.use(stylus.middleware({
+        src: __dirname + '/public',
         compile: compile
     }
 ));
+
 app.use(express.static(__dirname  + '/server/views'));
 
 
 app.get('*', function(req, res){
     res.render('index');
+});
+
+app.get('/partials/:partialPath', function(req, res){
+    res.render('/partials/' + req.param.partialPath);
 });
 
 var port = 3030;
