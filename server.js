e8eb7ba37e74ac551999d0836d9ad1270bb0d8a8
Vincent Zheng <zhengye1@mail.utoronto.ca>
2016-06-30 21:36:04 -0400

    Display the content, but chrome complain "Failed to parse SourceMap: http://localhost:3030/css/bootstrap.css.map"

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-'use strict';
-
 var express = require('express'),
     stylus = require('stylus'),
-    bodyParser = require('body-parser'),
     logger = require('morgan'),
-    path = require('path');
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -12,30 +10,28 @@ var app = express();
 
 function compile(str, path) {
     return stylus(str).set('filename', path);
-};
+}
 
-
-app.set('views', __dirname  + '/server/views');
+app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
 app.use(logger('dev'));
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(stylus.middleware({
+app.use(stylus.middleware(
+    {
         src: __dirname + '/public',
         compile: compile
     }
 ));
+app.use(express.static(__dirname + '/public'));
 
-app.use(express.static(__dirname  + '/server/views'));
+app.get('/partials/:partialPath', function(req, res) {
+    res.render('partials/' + req.params.partialPath);
+});
 
-
-app.get('*', function(req, res){
+app.get('*', function(req, res) {
     res.render('index');
-});
-
-app.get('/partials/:partialPath', function(req, res){
-    res.render('/partials/' + req.param.partialPath);
-});
+})
 
 var port = 3030;
 app.listen(port);
